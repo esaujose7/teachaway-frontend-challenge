b908e4adce62a25014d1e8f113255e29b2468848
Esaú Murillo <emurillo@artear.com>
2020-12-26 22:07:18 -0300

    implement the context usage on index and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,53 @@
-import { useState, useEffect, FC, ChangeEvent } from 'react';
-import { getImgurGallery, ImgurGallerySections, ImgurGallerySortValues, ImgurGalleryWindowOfTime } from './services/ImgurService';
-import { ImgurResponse, IGalleryImage, IGalleryAlbum } from './types';
+import { ImgurGallerySections, ImgurGallerySortValues, ImgurGalleryWindowOfTime } from './services/ImgurService';
 
-import GalleryImage from './components/GalleryImage';
-import GalleryAlbum from './components/GalleryAlbum';
+import GalleryFeed from './components/GalleryFeed';
+import Select from './components/Select';
 
-function onChanger(fn: Function) {
-  return (e: ChangeEvent<HTMLSelectElement>) => {
-    console.log(e.target.value);
-    fn(e.target.value);
-  }
-}
+import useImgurContext from './context/Imgur';
+
+import './App.css'
 
 const App = () => {
-  const [data, setData] = useState<ImgurResponse<(IGalleryImage | IGalleryAlbum)[]> | null>(null);
-  const [err, setErr] = useState<Error | null>(null);
-  const [loading, setLoading] = useState(true);
-  
-  // section, sort, window, viralImages 
-  const [section, setSection] = useState<ImgurGallerySections>(ImgurGallerySections.HOT);
-  const [sort, setSort] = useState<ImgurGallerySortValues>(ImgurGallerySortValues.VIRAL);
-  const [windowTime, setWindowTime] = useState<ImgurGalleryWindowOfTime>(ImgurGalleryWindowOfTime.DAY);
-  const [viralImages, setViralImages] = useState(true);
-
-  useEffect(() => {
-    getImgurGallery(section, sort, windowTime, viralImages)
-      .then(datilla => { setData(datilla); console.log(datilla); })
-      .catch((err) => { setErr(err); })
-      .finally(() => { setLoading(false) });
-
-    return () => {
-      setLoading(true);
-    };
-  }, [section, sort, windowTime, viralImages]);
-
-  if(loading) return <div>chance mano cargandiño</div>
-
-  if (err) return <div>no se mano paso algo mal fijate tu: {err.message}</div>
+  const {
+    state: { error, loading, section, sort, windowTime, viralImages, data: images },
+    actions: { setSection, setSort, setWindowTime, setViralImages }
+  } = useImgurContext();
 
   return (
-    <div className="App" style={{ display: 'flex', flexWrap: 'wrap' }}>
-      <header>
-        <select id="sort-select" name="sort-select" onChange={onChanger(setSection)} defaultValue="hot">
-          <option value="hot">hot</option>
-          <option value="user">user</option>
-        </select>
-      </header>
-      {Array.isArray(data?.data) && data?.data.map((item) => (
-        <div key={item.id} style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column', alignItems: 'center'}}>
-          {!item.is_album 
-              ? <GalleryImage image={item as IGalleryImage} />
-              : <GalleryAlbum album={item as IGalleryAlbum} />
-          }
-          <p style={{ maxWidth: '200px' }}>{item.title}</p>
-        </div>
-      ))}
+    <div className="wrapper">
+      {
+        error ? (
+          <div>something bad happened, please try again later. Error: {error.message}</div>
+        ) : (
+          <>
+            <header className="header">
+              <Select
+                id="section-select"
+                dispatcher={setSection}
+                values={Object.values(ImgurGallerySections)}
+                selected={section}
+              />
+              <Select
+                id="sort-select"
+                dispatcher={setSort}
+                values={Object.values(ImgurGallerySortValues)}
+                selected={sort}
+              />
+              <Select
+                id="window-select"
+                dispatcher={setWindowTime}
+                values={Object.values(ImgurGalleryWindowOfTime)}
+                selected={windowTime}
+              />
+              <input id="viralImagesCheckbox" type="checkbox" checked={viralImages} onChange={() => { setViralImages(viralImage => !viralImage) }} />
+              <label htmlFor="viralImagesCheckbox">viral images sis o non</label> 
+            </header>
+            <div style={{display:'flex', flexWrap: 'wrap'}}>
+              {loading ? <div> un chance manito ... </div> : <GalleryFeed items={images} />}
+            </div>
+          </>
+        )
+      }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ImgurContextProvider } from './context/Imgur';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ImgurContextProvider>
+      <App />
+    </ImgurContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
