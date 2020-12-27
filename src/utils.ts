539633e60e9ed2a32cc00e5572f10e683e5382aa
Esaú Murillo <emurillo@artear.com>
2020-12-26 22:06:10 -0300

    createCrx and ImgurJPG utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { createContext, useContext } from 'react';
+
 export async function api<T>(url: string, options: RequestInit | undefined = undefined): Promise<T> {
   try {
     const request = await fetch(process.env.REACT_APP_IMGUR_BASE_URL.concat(url), {
@@ -13,4 +15,19 @@ export async function api<T>(url: string, options: RequestInit | undefined = und
   } catch(e) {
     throw e; // Re throw so consumer can catch it
   }
+}
+
+// https://www.carlrippon.com/react-context-with-typescript-p4/
+export function createCtx<ContextType>() {
+  const ctx = createContext<ContextType | undefined>(undefined);
+  function useCtx() {
+    const c = useContext(ctx);
+    if (!c) throw new Error("useCtx must be inside a Provider with a value");
+    return c;
+  }
+  return [useCtx, ctx.Provider] as const;
+}
+
+export function getImgurImageJPG(id: string) {
+  return `https://i.imgur.com/${id}.jpg`;
 }
