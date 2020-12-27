81e07a2ab11f6b37ece6b79558f90e540a48af15
Esaú Murillo <emurillo@artear.com>
2020-12-27 02:49:11 -0300

    innerWidth hook

---
+++ src/hooks/useInnerWidth.ts
@@ -0,0 +1,24 @@
+import { useState, useLayoutEffect } from 'react';
+import debounce from 'lodash.debounce';
+
+const useInnerWidth = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  function handleResize() {
+    setInnerWidth(window.innerWidth);
+  }
+
+  const debouncedHandleResize = debounce(handleResize, 500);
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleResize);
+    }
+  }, [debouncedHandleResize]);
+
+  return innerWidth;
+}
+
+export default useInnerWidth;
