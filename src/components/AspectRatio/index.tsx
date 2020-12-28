37f8acbdfe1f808ec5739ed52e3fffabfb6c8d34
Esaú Murillo <emurillo@artear.com>
2020-12-28 05:16:59 -0300

    let AspectRatio accept className

--- src/components/AspectRatio/index.tsx
+++ src/components/AspectRatio/index.tsx
@@ -5,14 +5,15 @@ import './style.css';
 interface IAspectRatioProps {
   width: number,
   height: number,
+  className?: string
 }
 
 // https://css-tricks.com/aspect-ratio-boxes/#using-custom-properties
-const AspectRatio: FC<IAspectRatioProps> = ({ width, height, children }) => {
+const AspectRatio: FC<IAspectRatioProps> = ({ width, height, className = '', children }) => {
   const aspectRatioStyle = { '--aspect-ratio': (width / height).toFixed(2) } as CSSProperties;
 
   return (
-    <div className="aspect-ratio__container" style={aspectRatioStyle}>
+    <div className={`aspect-ratio__container ${className}`} style={aspectRatioStyle}>
       {children}
     </div>
   );
