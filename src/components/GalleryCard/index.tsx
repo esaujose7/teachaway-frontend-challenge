8cc88bb42229874b4fa6143cfc2f5ecff6c6861c
Esaú Murillo <emurillo@artear.com>
2020-12-27 21:52:22 -0300

    gallery card passes width to gallery image and delete some styles

--- src/components/GalleryCard/index.tsx
+++ src/components/GalleryCard/index.tsx
@@ -6,14 +6,15 @@ import './style.css';
 
 interface IGalleryCardProps {
   galleryItem: GalleryAlbumType | GalleryImageType;
+  width: number;
 }
 
-const GalleryCard: FC<IGalleryCardProps> = ({ galleryItem }) => (
+const GalleryCard: FC<IGalleryCardProps> = ({ galleryItem, width }) => (
   <div className="gallery-card">
     {
       galleryItem.is_album
-        ? <GalleryImage image={galleryItem.images[0] as GalleryImageType} />
-        : <GalleryImage image={galleryItem} />
+        ? <GalleryImage image={galleryItem.images[0] as GalleryImageType} displayWidth={width} />
+        : <GalleryImage image={galleryItem} displayWidth={width}/>
     }
     <span className="gallery-description">{galleryItem.title}</span>
   </div>
