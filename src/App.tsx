8f21969b49261f799e0e52764034200cadd3d92e
Esaú Murillo <emurillo@artear.com>
2020-12-27 23:22:03 -0300

    clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,27 @@
-import { ImgurGallerySections, ImgurGallerySortValues, ImgurGalleryWindowOfTime  } from './types';
-
-import useImgurContext from './context/Imgur';
+import { useImgurState } from './context/Imgur';
 import GalleryFeed from './components/GalleryFeed';
-import Select from './components/Select';
-import FormControl from './components/FormControl';
+import GalleryControls from './components/GalleryControls';
 import Error from './components/Error';
 import Loading from './components/Loading';
 
 import './App.css'
 
 const App = () => {
-  const {
-    state: { error, loading, section, sort, windowTime, viralImages, data: images },
-    actions: { setSection, setSort, setWindowTime, setViralImages }
-  } = useImgurContext();
+  const { error, loading, images, selectedImage } = useImgurState();
+
+  if (error) {
+    return (
+      <div className="wrapper">
+        <Error error={error} />
+      </div>
+    );
+  }
 
   return (
     <div className="wrapper">
-      {error
-        ? <Error error={error} />
-        : <>
-          <header className="header">
-            <Select id="section-select" dispatcher={setSection} values={Object.values(ImgurGallerySections)} selected={section}>
-              Choose the section of the gallery
-            </Select>
-            <Select id="sort-select" dispatcher={setSort} values={Object.values(ImgurGallerySortValues)} selected={sort}>
-              Sort images by
-            </Select>
-            <Select id="window-select" dispatcher={setWindowTime} values={Object.values(ImgurGalleryWindowOfTime)} selected={windowTime}>
-              Select the window of time for the gallery
-            </Select>
-            <FormControl direction="row">
-              <input id="viralImagesCheckbox" type="checkbox" checked={viralImages} onChange={() => { setViralImages(viralImage => !viralImage) }} />
-              <label htmlFor="viralImagesCheckbox" className="bold ml-10">Show viral images?</label> 
-            </FormControl>
-          </header>
-          {loading ? <Loading /> : <GalleryFeed items={images} />}
-        </>
-      }
+      {selectedImage && <div>{JSON.stringify(selectedImage, null, 2)}</div>}
+      <GalleryControls />
+      {loading ? <Loading /> : <GalleryFeed items={images} />}
     </div>
   );
 }
